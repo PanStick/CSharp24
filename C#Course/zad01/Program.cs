555cc9071c5b6f3bb762da32fa87f02cc93429b7
PanStick <tymoteusz.blaszcz@student.uj.edu.pl>
2024-10-31 22:17:13 +0100

    Changed startup file logic

---
+++ C#Course/zad01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Course.zad01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //zad 1
+            //Game g = new();
+            //g.Start();
+        }
+    }
+}
